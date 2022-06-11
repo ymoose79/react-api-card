1d6cb3a69f91298b4d061205fcc89c7babae2e09
ymoose79 <justin.r.stock@icloud.com>
2022-06-11 12:31:53 -0400

    waiting on image resize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 // import SearchBar from "./component/SearchBar"
 import StudentCard from "./component/StudentCard"
+import "./component/CSS/app.css"
 
 function App() {
 
@@ -20,9 +21,15 @@ function App() {
   const handleInputChange = (e) => { setStudentName(e.target.value) }
 
   return (
-    <div className="container-md my-5">
-      <div className="input-group">
-        <input type="text" className="form-control" placeholder='Search by name' value={studentName} onChange={handleInputChange} />
+    <div className="container my-5 bg-light rounded-2 shadow-lg" style={{ maxWidth: "640px" }} id="container">
+      <div className="row justify-content-center">
+        <div className="col bg-light border-bottom" style={{ maxWidth: "640px" }}>
+          <div className="form-floating">
+            <div className="input-group my-1 border-botom" >
+              <input type="text" className="form-control-plaintext" placeholder='Search by name' value={studentName} onChange={handleInputChange} />
+            </div>
+          </div>
+        </div>
       </div>
       {students.filter((student) => {
         if (studentName === "") {
--- src/component/StudentCard.js
+++ src/component/StudentCard.js
@@ -17,30 +17,30 @@ const StudentCard = (student) => {
 
 
     return (
-        <>
-            <div className="card mx-auto border-light" style={{ maxWidth: "540px" }}>
-                <div className="row g-0">
-                    <div className="col-4 text-center align-self-center">
-                        <img src={pic} className="img-fluid rounded-circle border border-dark border" alt="..." style={{ width: "65%" }} />
+        <div className='border-bottom'>
+            <div className="card mx-auto bg-light" style={{ maxWidth: "640px" }}>
+                <div className="row g-0 ">
+                    <div className="col-md-2 mt-sm-2 align-self-center ">
+                        <img src={pic} className="img-fluid rounded-circle border mx-auto
+                    " alt="..." style={{ width: "85%" }} />
                     </div>
-                    <div className="col">
+                    <div className="col-9 col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">{firstName} {lastName}</h5>
-                            <h6 className="card-text"><small className="text-muted">Email: {email}</small></h6>
-                            <h6 className="card-text"><small className="text-muted">Company: {company}</small></h6>
-                            <h6 className="card-text"><small className="text-muted">Skil: {skill}</small></h6>
-                            <h6 className="card-text"><small className="text-muted">Average: {avg}</small></h6>
-
+                            <h5 className="card-title display-6 fw-bold">{firstName} {lastName}</h5>
+                            <h6 className="card-text ms-md-2" id="text"><small className="text-muted">Email: {email}</small></h6>
+                            <h6 className="card-text ms-md-2" id="text"><small className="text-muted">Company: {company}</small></h6>
+                            <h6 className="card-text ms-md-2" id="text"><small className="text-muted">Skill: {skill}</small></h6>
+                            <h6 className="card-text ms-md-2" id="text"><small className="text-muted">Average: {avg}%</small></h6>
                         </div>
                     </div>
-                    <div className="col-auto">
+                    <div className="col mt-1 justify-content-end">
                         {!isOpen &&
-                            <button className='btn text-secondary' data-toggle="collapse" data-target="#showGrades" onClick={() => setIsOpen(!isOpen)}>  <FontAwesomeIcon icon={faPlus} size="2x" /></button>}
+                            <button className='btn text-secondary mt-1 mt-md-2 ps-md-5' data-toggle="collapse" data-target="#showGrades" onClick={() => setIsOpen(!isOpen)}>  <FontAwesomeIcon icon={faPlus} size="2x" id="icon" /></button>}
                         {isOpen &&
-                            <button className='btn text-secondary hover-primary' data-toggle="collapse" data-target="#showGrades" onClick={() => setIsOpen(!isOpen)}>  <FontAwesomeIcon icon={faMinus} size="2x" /></button>}
+                            <button className='btn text-secondary mt-1 mt-md-2 ps-md-5' data-toggle="collapse" data-target="#showGrades" onClick={() => setIsOpen(!isOpen)}>  <FontAwesomeIcon icon={faMinus} size="2x" id="icon" /></button>}
                     </div>
                 </div>
-                <div class="row justify-content-center">
+                <div className="row justify-content-center">
                     <div className='col-4 offset-1'>
                         {isOpen && grades.map((grade, i) => {
                             return <h6 className="card-text"><small className="text-muted">Test {i + 1}: {grade}</small></h6>
@@ -48,8 +48,7 @@ const StudentCard = (student) => {
                     </div>
                 </div>
             </div>
-            <hr />
-        </>
+        </div>
     )
 }
 
